7b019e3e400925891262322e99feab2dfb11f698
= <=>
2021-10-08 01:38:11 -0500

    estilo bicicletas

--- Frot-end2/cliente/src/app/app.module.ts
+++ Frot-end2/cliente/src/app/app.module.ts
@@ -11,12 +11,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { FooterComponent } from './components/footer/footer.component'; 
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearProductoComponent,
-    ListarProductosComponent
+    ListarProductosComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
